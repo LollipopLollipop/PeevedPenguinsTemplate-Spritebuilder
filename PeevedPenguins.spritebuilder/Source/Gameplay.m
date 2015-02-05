d3b0b22caf18b567d0bd008a0858615fa4e14c8a
LollipopLollipop <jodie.d.zhao@gmail.com>
2015-02-05 18:31:57 -0500

    Automatically Committed for MakeGamesWithUs

--- PeevedPenguins.spritebuilder/Source/Gameplay.m
+++ PeevedPenguins.spritebuilder/Source/Gameplay.m
@@ -12,6 +12,7 @@
     CCPhysicsNode *_physicsNode;
     CCNode *_catapultArm;
     CCNode *_levelNode;
+    CCNode *_contentNode;
 }
 
 // is called when CCB file has completed loading
@@ -42,9 +43,15 @@
     [penguin.physicsBody applyForce:force];
     
     // ensure followed object is in visible are when starting
+    /*
     self.position = ccp(0, 0);
     CCActionFollow *follow = [CCActionFollow actionWithTarget:penguin worldBoundary:self.boundingBox];
-    [self runAction:follow];
+    [self runAction:follow];*/
+    
+    // ensure followed object is in visible are when starting
+    self.position = ccp(0, 0);
+    CCActionFollow *follow = [CCActionFollow actionWithTarget:penguin worldBoundary:self.boundingBox];
+    [_contentNode runAction:follow];
 }
 
 // called when press the "reset" button
